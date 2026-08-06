b7ccc338c4dd6cbd78da28d14cf12b83acb658a6
Mariano <mlamaison@frba.utn.edu.ar>
2026-08-06 13:01:56 -0300

    Finished chapter 1

--- src/answers/chapter_1/closingExcerisices.c
+++ src/answers/chapter_1/closingExcerisices.c
@@ -2,6 +2,12 @@
 #define MAXLINE 1000
 #define TABSIZE 8
 	
+#define BAD_SINGLE_COMENT 0
+#define BAD_PAIRING 1
+#define ALL_GOOD 2
+#define BAD_CHAR_CONST 3
+#define BAD_STRING_CONST 4
+#define ILEGAL_STATE 5
 int getline(char line[], int maxline);
 void copy(char to[], char from[]);
 void detab(char s[], int size);
@@ -10,18 +16,36 @@ void fold(char s[], int size, int lineLengt);
 void deComenter(char s[], int size);
 void append(char s[], char extra[]);
 void getTextBlock(char textBlock[]);
+int errorChecker(char textBlock[], int row, int column);
+void printErrorMessage(int errorCode);
 int main(void){
 	char line[MAXLINE];
 	char textBlock[MAXLINE];
 	getTextBlock(textBlock);
-	deComenter(textBlock, MAXLINE);
-	printf("The de comented code looks like this \n%s\n", textBlock);
+	printErrorMessage(errorChecker(textBlock, 0, 0));
+//	deComenter(textBlock, MAXLINE);
+//	printf("The de comented code looks like this \n%s\n", textBlock);
 //	while(getline(line, MAXLINE) > 0){
 //		detab(line, MAXLINE);
 //		fold(line, MAXLINE, 8);
 //		printf("The folder line looks like this \n%s\n", line); 
 //	}
 	return 0;
+}
+
+void printErrorMessage(int errorCode){
+	if (errorCode==BAD_SINGLE_COMENT)
+		printf("Error inclomplete single coment\n");
+	else if(errorCode==BAD_PAIRING)
+		printf("Error incomplete (, { or [ secuence\n");
+	else if(errorCode==BAD_CHAR_CONST)
+		printf("Error incorrect char constant");
+	else if(errorCode==BAD_STRING_CONST)
+		printf("Error incorrect string constant");
+	else if(errorCode==ILEGAL_STATE)
+		printf("Either a constant or a coment were never finished");
+	else if(errorCode==ALL_GOOD)
+		printf("There were no basic errors on the code");
 }
 int getline(char s[], int lim){
 	int c, i;
@@ -200,4 +224,90 @@ void getTextBlock(char textBlock[]){
 	textBlock[0] = '\0';
 	while(getline(line, MAXLINE)>0)
 		append(textBlock, line);
+}
+
+int indexForChar(int c){
+	if (c=='('||c==')')
+		return 0;
+	else if(c=='{'||c=='}')
+		return 1;
+	else if(c=='['||c==']')
+		return 2;
+	else 
+		return -129;
+}
+
+int errorChecker(char t[], int row, int column){
+#define GENERAL_CHECK 0
+#define CHAR_CONST_CHECK 1
+#define STRING_CONST_CHECK 2
+#define MULTI_LINE_COMENT_CHECK 3
+	int i, c, state, lengt;
+	int paringCounter[3];
+	for (i=0; i<3; ++i)
+		paringCounter[i]=0;
+	state = GENERAL_CHECK;
+	for (i=0; (c=t[i])!='\0';++i)
+		if(state == GENERAL_CHECK)
+		{
+			if(c=='\'')
+			{
+				lengt = 0;
+				state=CHAR_CONST_CHECK;
+			}
+			else if(c=='"')
+				state=STRING_CONST_CHECK;
+			else if(c=='/')
+			{
+				if(t[i+1]=='*')
+					state=MULTI_LINE_COMENT_CHECK;
+				else if (t[i+1]!='/')
+					return BAD_SINGLE_COMENT;
+				else 
+					++i;//so whe skip what was already checked 
+			}else if(c=='{' || c=='(' || c=='[')
+				++paringCounter[indexForChar(c)];
+			else if(c=='}' || c==')' || c==']')
+			{
+				--paringCounter[indexForChar(c)];
+				if (paringCounter[indexForChar(c)]<0)
+					return BAD_PAIRING;
+			}
+		}else if(state == CHAR_CONST_CHECK){
+			if (lengt > 2)
+				return BAD_CHAR_CONST;
+			if (c=='\'')
+				state = GENERAL_CHECK;
+			if (c=='\\'){
+				if (t[i+1]==' ' || t[i+1]=='\t' || t[i+1]=='\n')
+					return BAD_CHAR_CONST;
+				else
+					++i;//also to avoid the case wehre '\'' crashing 
+			}
+			++lengt;
+		}else if(state == STRING_CONST_CHECK){
+			if (c=='"')
+				state = GENERAL_CHECK;
+			if (c=='\\')
+			{
+				if (t[i+1]==' ' || t[i+1]=='\t' || t[i+1]=='\n')
+					return BAD_STRING_CONST;
+				else
+					++i;
+			}
+			if (c=='\n')
+				return BAD_STRING_CONST;
+		}else if(state == MULTI_LINE_COMENT_CHECK){
+			if(c=='*')
+				if (t[i+1]=='/'){
+					++i;
+					state = GENERAL_CHECK;
+				}
+		}
+	for(i=0; i<3; ++i)
+		if (paringCounter[i]!=0)
+			return BAD_PAIRING;
+	if (state != GENERAL_CHECK)//this is the only state that woul signify a normal ending
+		return ILEGAL_STATE;
+	return ALL_GOOD;
 }
